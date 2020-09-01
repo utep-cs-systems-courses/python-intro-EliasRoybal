f41b3dfd30a82ca1a6b15b560809b040a470ee03
EliasRoybal <eroybal@miners.utep.edu>
2020-09-01 03:09:08 -0600

    List of words with occurences from input file prints with bug that counts white space as a word

--- wordCount.py
+++ wordCount.py
@@ -1,1 +1,32 @@
 import re
+import sys
+
+#Takes arguments from command line and set input and output files
+inputTextfile = sys.argv[1]
+outputFile = sys.argv[2]
+
+#opens file to read
+inputFile = open(inputTextfile, "r")
+
+#Dictionary created to store words
+wordDict = dict()
+
+#Goes through file line by line
+for line in inputFile:
+    #gets rid of any characters for new line
+    line = line.strip()
+    #this splits at any punctuation or whitespace
+    wordsInLine = re.split('[ \t]',line)
+
+    #This iterates through each word in every line read
+    for word in wordsInLine:
+        if word in wordDict:
+            #increments the word count if word is already in the dictionary
+            wordDict[word] = wordDict[word]+1
+        else:
+            #adds the new  word to the  dictionary
+            wordDict[word] = 1
+
+#Tests out that some list prints out
+for key in list(wordDict.keys()):
+    print(key, ":", wordDict[key])
