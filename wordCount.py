914076c321f0d0e1478a26126158f452780835a6
EliasRoybal <eroybal@miners.utep.edu>
2020-09-01 22:58:21 -0600

    Fixed issue where words with apostrophes or dashes wouldn't be added to the list correctly. All tests passed.

--- wordCount.py
+++ wordCount.py
@@ -16,13 +16,16 @@ for line in inputFile:
     #removes any whitespace and makes all words lowercase
     line = line.strip()
     line = line.lower()
+    #removes apostrophes or dashes and makes them white space
+    line = line.replace("'",' ')
+    line = line.replace("-",' ')
     
-    #this splits at any punctuation
+    #this splits at any whitespace
     wordsInLine = line.split()
     
     #This iterates through each word in every line read
     for word in wordsInLine:
-        #Removes any white space or punctuation
+        #Removes any numbers from the word and replaces with whitespace
         word = re.sub('[^A-Za-z0-9]+','',word)
 
         if word in wordDict:
@@ -30,20 +33,15 @@ for line in inputFile:
             wordDict[word] = wordDict[word]+1
 
         else:
-            #adds the new  word to the  dictionary
+            #adds  new  word to the dictionary and sets counter to 1
             wordDict[word] = 1
 
-wordList = list(wordDict.keys())
-wordList.sort()
-
-
+#opens file to be able to write to it
 outputFile = open(outputTextfile, 'w')
 
 #Writes sorted words and counter to file
 for key in sorted(wordDict):
-    #converts line with word and counter to string so it becomes writable
+    #converts line with word and counter to string so it becomes writable to file
     wordDictString = str(key)+ " " + str(wordDict[key])
     outputFile.write(wordDictString)
     outputFile.write("\n")
-   # outputFile.write(key)
-   # outputFile.write(wordDict[key])
