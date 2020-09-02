edd4e857cfb511a910c0d485a567bbc192e2a71f
EliasRoybal <eroybal@miners.utep.edu>
2020-09-01 19:48:04 -0600

    Reverted unwanted changes

--- wordCount.py
+++ wordCount.py
@@ -1,8 +1,8 @@
 import re
 import sys
-
-#Takes arguments from command line to set input file
+#Takes arguments from command line and set input and output files
 inputTextfile = sys.argv[1]
+outputFile = sys.argv[2]
 
 #opens file to read
 inputFile = open(inputTextfile, "r")
@@ -14,17 +14,24 @@ wordDict = dict()
 for line in inputFile:
     #gets rid of any characters for new line
     line = line.strip()
+    
     #this splits at any punctuation or whitespace
     wordsInLine = re.split('[ \t]',line)
-
+    
     #This iterates through each word in every line read
     for word in wordsInLine:
         if word in wordDict:
+            # word = word.strip(".,!?:;'\")
             #increments the word count if word is already in the dictionary
             wordDict[word] = wordDict[word]+1
         else:
             #adds the new  word to the  dictionary
             wordDict[word] = 1
-
-wordList = list(wordDict.keys())
-wordList.sort()
+            
+            wordList = list(wordDict.keys())
+            wordList.sort()
+            
+        #Tests out that some list prints out
+        for key in list(wordDict.keys()):
+            for key in wordList:
+                print(key, ":", wordDict[key])
